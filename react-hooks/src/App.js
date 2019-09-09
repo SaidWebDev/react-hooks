b5f3fd9620899fe2ed2f0284b0eb8dfcdb9dd9b9
said moutaa <smoutaa@gmail.com>
2019-09-09 23:53:46 +0200

    rewrite two states

--- react-hooks/src/App.js
+++ react-hooks/src/App.js
@@ -1,21 +1,17 @@
-import React ,{useState} from 'react'
+import React , {useState} from 'react'
 
 const App=() =>{
-  const [{count, count2}, setCount]= useState({count:10, count2:20})
+  const [count, setCount]= useState(10);
+  const [count2, setCount2]= useState(20);
   return (
     <div>
-      <button onClick={
-        ()=>setCount(
-          currentState=>(
-            {
-              ...currentState,
-              count: currentState.count +1
-            }
-          )
-        )
-      }>+</button>
-      <div> count : {count}</div>
-      <div> count2 : {count2}</div>
+      <button onClick=
+      {()=>{
+      setCount(c=> c +1);
+      setCount2(c=> c+10)
+      }}> +</button>
+      <div> count: {count}</div>
+      <div> count2: {count2}</div>
     </div>
   )
 }
