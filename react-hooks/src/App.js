e3aee2b78afe32abd3de4738cef09d9243b53028
said moutaa <smoutaa@gmail.com>
2019-09-09 23:31:14 +0200

    setState for one count

---
+++ react-hooks/src/App.js
@@ -0,0 +1,12 @@
+import React , {useState} from 'react'
+const App =()=> {
+  const [count, setCount]= useState(10);
+  return (
+
+    <div>
+      <button onClick={()=>setCount(count +1)}>+</button>
+      <div>{count}</div>
+    </div>
+  )
+}
+export default App;
