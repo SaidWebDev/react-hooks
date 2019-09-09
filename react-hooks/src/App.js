27607a4be32149f2151c3ab6b948214cdc4b2a14
said moutaa <smoutaa@gmail.com>
2019-09-09 23:44:42 +0200

    adding two states

--- react-hooks/src/App.js
+++ react-hooks/src/App.js
@@ -1,12 +1,22 @@
-import React , {useState} from 'react'
-const App =()=> {
-  const [count, setCount]= useState(10);
-  return (
+import React ,{useState} from 'react'
 
+const App=() =>{
+  const [{count, count2}, setCount]= useState({count:10, count2:20})
+  return (
     <div>
-      <button onClick={()=>setCount(count +1)}>+</button>
-      <div>{count}</div>
+      <button onClick={
+        ()=>setCount(
+          currentState=>(
+            {
+              ...currentState,
+              count: currentState.count +1
+            }
+          )
+        )
+      }>+</button>
+      <div> count : {count}</div>
+      <div> count2 : {count2}</div>
     </div>
   )
 }
-export default App;
+export default  App;
