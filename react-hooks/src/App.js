85a86885f41a222183cc7a5cac5efd10e447f9e2
said moutaa <smoutaa@gmail.com>
2019-09-10 23:13:46 +0200

    Adding mount 1 and mount 2 differnetly

--- react-hooks/src/App.js
+++ react-hooks/src/App.js
@@ -3,12 +3,33 @@ import {useForm} from "./useForm";
 import {Hello} from "./Hello"
 
 const App=() =>{
-  const [values, handleChange]= useForm({email:'', password:'', firstName:''});
-  const [showHello, setShowHello]= useState(true);
+   const [values, handleChange]= useForm({email:'', password:'', firstName:''});
+  // // const [showHello, setShowHello]= useState(true);
+
+  // useEffect(()=>{
+  //   // console.log("render");
+  //   const onMouseMove =e => {
+  //     console.log(e);
+  //   }
+  //   window.addEventListener("mousemove", onmousemove);
+
+  //   return()=> {
+  //       window.removeEventListener("mousemove", onMouseMove);
+  //   }
+  // }, [ ]);
+
+  useEffect(()=>{
+    console.log('mount1');
+  }, [])
+
+  useEffect(()=>{
+    console.log('mount2');
+  }, [])
 
   return (
     <div>
- 
+      {/* <button onClick={() => setShowHello(!showHello)}>toggle</button>
+      {showHello && <Hello/>} */}
       <input
          name="email"
          placeholder="email"
